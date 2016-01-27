977247ed07b1d7ba78b37cc4d17358feb8482a9e
Zeeshan Ahmad <zeeshan.ahmad@grofers.com>
2016-01-27 12:04:42 +0530

    static directory changed

--- blog/settings.py
+++ blog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 #AUTH_USER_MODEL = 'authentication.Account'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
