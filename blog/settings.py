0ef8ec9cf624c73ed3dfef2b404087f2b6615ee9
Zeeshan Ahmad <zeeshan.ahmad@grofers.com>
2016-01-27 11:58:55 +0530

    template directory changed

--- blog/settings.py
+++ blog/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/Zeeshan_Ahmad/django_project/blog/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
