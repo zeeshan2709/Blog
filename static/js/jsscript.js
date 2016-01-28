7f54fbaebf68e9cb604dd62a9459b4367765709a
Zeeshan Ahmad <zeeshan.ahmad@grofers.com>
2016-01-28 15:04:50 +0530

    trimmed spaces from forms

--- static/js/jsscript.js
+++ static/js/jsscript.js
@@ -1,6 +1,6 @@
 function login_validate(){
-	var x = document.forms["logform"]["user"].value;
-	var y = document.forms["logform"]["pass"].value;
+	var x = document.forms["logform"]["user"].value.trim();
+	var y = document.forms["logform"]["pass"].value.trim();
 	if(x == "" || y == "" || x == null || y == null){
 		alert("Enter both username and password");
 		return false;
@@ -8,9 +8,9 @@ function login_validate(){
 }
 
 function post_validate(){
-	var x = document.forms["postform"]["title"].value;
-	var y = document.forms["postform"]["descrip"].value;
-	var z = document.forms["postform"]["content"].value;
+	var x = document.forms["postform"]["title"].value.trim();
+	var y = document.forms["postform"]["descrip"].value.trim();
+	var z = document.forms["postform"]["content"].value.trim();
 	if(x == "" || y == "" || z == "" || x == null || y == null || z == null){
 		document.getElementById("pst").action="#";
 		alert("All fields are compulsary!!!");
